920a1392f2a0aa269194e07aa84c7d50787fffd1
jeffersondossantosveiga@gmail.com <86816865+jehveiga@users.noreply.github.com>
2022-02-17 17:02:47 -0300

    Jogada especial promocao

--- jogo_xadrez-console/xadrez/PartidaDeXadrez.cs
+++ jogo_xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -143,7 +143,23 @@ namespace xadrez
                 DesfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
-            if(EstaEmXeque(Adversaria(JogadorAtual)))
+
+            Peca p = Tab.Peca(destino);
+
+            // #jogaEspecial promocao
+            if(p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
+            if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
             }
@@ -161,8 +177,6 @@ namespace xadrez
                 Turno++;
                 MudaJogador();
             }
-
-            Peca p = Tab.Peca(destino);
 
             //# JogadaEspecial En Passant
             if(p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
